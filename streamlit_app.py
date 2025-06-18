e576d85dcc3642e51fc04e16a7aa05276ae55149
Ct Nr-Ftmh <71620819+ctmitly@users.noreply.github.com>
2025-06-19 05:37:58 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ def get_questions(amount=5, category=None, difficulty=None):
 
 # Setup app title
 st.title("Random Quiz App 🧠")
-st.image("quiz app.png", use_column_width=True)
+st.image("quiz app.png")
 
 # Category and difficulty selection only shown before quiz starts
 if 'questions' not in st.session_state:
